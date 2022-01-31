fe10e7496aaea1736343cf05b15f7fee85004c88
Sandeep Gautam <sandeepgautam568@gmail.com>
2022-01-31 08:43:08 +0545

    Initial Commit

---
+++ Locatly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Locatly
+//
+//  Created by Sandeep Gautam on 31/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
